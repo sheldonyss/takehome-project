53eb0d0c54da9b3f23a4b5e9e80b3f05dc94882d
sheldonyss <sheldon.yss@gmail.com>
2021-09-12 21:27:55 +0800

    initial version

---
+++ StripeBookStore/Controllers/CheckoutController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Stripe;
+
+namespace StripeBookStore.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public CheckoutController()
+        {
+            StripeConfiguration.ApiKey =
+                "sk_test_51JY50UEn75gMW8WBoxBSzPWjZb2Wkmi6rW8zNjRXR81bosxyaUUODGvc1QezXmsjTP1W6FCAkyzJV6jusWqsXfGx00ovOACDff";
+
+        }
+        public IActionResult Index(int item)
+        {
+            string title = string.Empty, error = string.Empty;
+            int amount = 0;
+            if (item < 1 || item > 3)
+            {
+                return NotFound();
+            }
+            switch (item)
+            {
+                case 1:
+                    title = "The Art of Doing Science and Engineering";
+                    amount = 2300;
+                    break;
+                case 2:
+                    title = "The Making of Prince of Persia: Journals 1985-1993";
+                    amount = 2500;
+                    break;
+                case 3:
+                    title = "Working in Public: The Making and Maintenance of Open Source";
+                    amount = 2800;
+                    break;
+                default:
+                    // Included in layout view, feel free to assign error
+                    error = "No item selected";
+                    break;
+
+            }
+
+            ViewData["amount"] = amount;
+            ViewData["title"] = title;
+            ViewData["error"] = error;
+            ViewData["secret"] = CreateIntendClientSecret(amount);
+            return View();
+        }
+
+
+
+        public IActionResult Success(string id)
+        {
+            try
+            {
+                var service = new PaymentIntentService();
+                var intent = service.Get(id);
+                var amount = intent.AmountReceived / 100;
+                ViewData["status"] = intent.Status;
+                ViewData["method"] = string.Join(",", intent.PaymentMethodTypes);
+                ViewData["amount"] = amount.ToString("C");
+                ViewData["id"] = intent.Id;
+            }
+            catch (StripeException ex)
+            {
+                return NotFound();
+            }
+
+            return View();
+        }
+
+        private string CreateIntendClientSecret(int amount)
+        {
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = amount,
+                Currency = "sgd",
+                // Verify your integration in this guide by including this parameter
+                Metadata = new Dictionary<string, string>
+                {
+                    { "integration_check", "accept_a_payment" },
+                }
+                
+            };
+
+            var service = new PaymentIntentService();
+            var paymentIntent = service.Create(options,new RequestOptions()
+            {
+                // in real life, use customer's order number as the key
+                // IdempotencyKey = 
+            });
+            return paymentIntent.ClientSecret;
+        }
+    }
+}
---
+++ StripeBookStore/readme.md
@@ -0,0 +1,17 @@
+﻿
+  
+
+# Assumption
+
+This project assumes the following are true:
+
+1.  The currency is fixed to USD. To support other currencies, an utility class is desired to handle the currency display from its smallest unit.
+2.  For simplicity, this project does not include customer membership management, order management, and other commonly seen features provided in e-commerce software. This means you can pay for the same item many times.
+
+  
+
+# Certificate Issues
+
+This project uses a self-signed certificate and it is not trusted by the Chrome browser by default.
+
+You must **manually trust the self-signed certificate** first before you can try the payment flow. See https://www.pico.net/kb/how-do-you-get-chrome-to-accept-a-self-signed-certificate/ for the details.
